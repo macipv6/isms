e96ac47b47e549de429a927496aa517ed8b8a59a
macipv6 <it@sebastian-schulz.info>
2026-09-04 14:43:40 +0200

    fix: document measure status type

--- app/Models/Measure.php
+++ app/Models/Measure.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/** @property MeasureStatus $status */
 class Measure extends Model
 {
     /** @use HasFactory<MeasureFactory> */
